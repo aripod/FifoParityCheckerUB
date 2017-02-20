ce73b974b0d3f17d5c9abeb72c9338d5ebae0d48
Ariel Podlubne <ariel.podlubne@laas.fr>
2017-02-20 10:59:49 -0500

    Fixed checker to count pass and drop only when the test is running (CE=1)

--- Checker/Checker.vhd
+++ Checker/Checker.vhd
@@ -29,116 +29,29 @@ architecture Behavioral of Checker is
 	signal result_passed : STD_LOGIC_VECTOR(7 downto 0) := (others=>'0');				-- MAX value 255. Thereofre, TEST_LENGTH has to be equal or less than that.
 	signal result_dropped : STD_LOGIC_VECTOR(7 downto 0) := (others=>'0');				-- MAX value 255. Thereofre, TEST_LENGTH has to be equal or less than that.
 	signal result_loss : STD_LOGIC_VECTOR(7 downto 0) := (others=>'0');				-- MAX value 255. Thereofre, TEST_LENGTH has to be equal or less than that.
-	signal saved: std_logic := '0';
 	
 	signal count_pass_en : std_logic :='0';
 	signal count_drop_en : std_logic :='0';
 	
-	signal pass : STD_LOGIC_VECTOR (FIFO_WIDTH-1 downto 0) :=(others=>'0');
-	signal present : STD_LOGIC_VECTOR (FIFO_WIDTH-1 downto 0) :=(others=>'0');
-	signal aux : STD_LOGIC_VECTOR (FIFO_WIDTH-1 downto 0) :=(others=>'0');
-	
-	signal stest: std_logic :='0';
-	
 begin
 
 	
-	process(clk, grant_i, valid_o)
+	-- ** Process to count passed and dropped data ** --
+	process(clk, ce, grant_i, valid_o)
 		begin
-		if(clk'event and clk='0') then
-			stest <= not stest;
-			if(grant_i='1') then
-				if(valid_o='1') then
-					result_passed <= result_passed + 1;
-				else
-					result_dropped <= result_dropped + 1;
+		if(clk'event and clk='0') then	-- As data is poped on rising edge, at falling edge data is already poped and available.
+			if(ce='1') then					-- Only check if the test is being performed.
+				if(grant_i='1') then			-- And if the receiver wants to read data.
+					if(valid_o='1') then		-- Output from ParityCheck
+						result_passed <= result_passed + 1;
+					else
+						result_dropped <= result_dropped + 1;
+					end if;
 				end if;
 			end if;
 		end if;
 	end process;
-	test <= stest;
 	passed_o <= result_passed;
 	dropped_o <= result_dropped;
-	
---	process(clk, ce)		-- Process to store PUSHED and POPED data and to analyze lost data.
---		begin
---		if(clk'event and clk='1') then
---			if(ce='1') then			-- Collects data
---				if(valid_i='1' and grant_o/='1') then	-- If something wants to be pushed (valid_i=1) and the fifo can take it (grant_o/=0).
---					if(to_integer(unsigned(pushed_index)) < TEST_LENGTH) then
---						pushed(to_integer(unsigned(pushed_index))) <= data_i;
---						pushed_index <= pushed_index + 1;
---					end if;
---				end if;
---				
---				if(grant_i='1') then				-- If it is set to pop (It cannot check for valid_o as it might be 0 due to ParityCheck).
---					if(to_integer(unsigned(poped_index)) < TEST_LENGTH) then
---						pushed(to_integer(unsigned(poped_index))) <= data_o;
---						poped_index <= poped_index + 1;
---					end if;
---					if(valid_o='1') then
---						result_passed <= result_passed + 1;
---					else
---						result_dropped <= result_dropped + 1;
---					end if;
---				end if;
---			else							-- Analysis data
---				for i in 0 to TEST_LENGTH-1 loop
---					if(pushed(to_integer(unsigned(pushed_index))) /= pushed(to_integer(unsigned(poped_index)))) then
---						result_dropped <= result_dropped + 1;
---					end if;
---				end loop;
---				pushed_index <= (others=>'0');
---				poped_index <= (others=>'0');
---			end if;
---		end if;
---	end process;
---	
---	passed_o <= result_passed;
---	dropped_o <= result_dropped;
---	loss_o <= result_loss;
-	
---	process(ce, data_i)		-- Process to store data that is PUSHED.
---		begin
---		if(ce='1') then
---			if(saved='0') then
---				saved <= '1';
---			else
---				if(to_integer(unsigned(pushed_index)) < TEST_LENGTH) then
---					if(valid_i='1' and grant_o/='0') then
---						pushed(to_integer(unsigned(pushed_index))) <= data_i;
---						pushed_index <= pushed_index + 1;
---					end if;
---				end if;
---			end if;
---		end if;
---	end process;
---	
-
-
-	
---	process(ce, data_o)		-- Process to store data that is POPED.
---		begin
---		if(ce='1') then
---			if(saved='0') then
---				saved <= '1';
---			else
---				if(to_integer(unsigned(poped_index)) < TEST_LENGTH) then
---					if(grant_i='1') then
---						poped(to_integer(unsigned(poped_index))) <= data_o;
---						if(poped(to_integer(unsigned(poped_index))) /= pushed(to_integer(unsigned(poped_index)))) then
---							result_loss <= result_loss + 1;
---						end if;
---						poped_index <= poped_index + 1;
---					end if;
---				if(valid_o='1') then
---						result_passed <= result_passed + 1;
---					else
---						result_dropped <= result_dropped + 1;
---					end if;
---				end if;
---			end if;
---		end if;
---	end process;
 	
 end Behavioral;
