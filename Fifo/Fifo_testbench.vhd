c2113380f01f88c6535078b6510152f41c77619a
Ariel Podlubne <ariel.podlubne@laas.fr>
2017-02-21 12:07:53 -0500

    Fifo module finished

--- Fifo/Fifo_testbench.vhd
+++ Fifo/Fifo_testbench.vhd
@@ -1,36 +1,13 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   15:20:19 02/14/2017
--- Design Name:   
--- Module Name:   /home/ariel/Documents/VHDL/FifoParityCheckerUB/Fifo/Fifo/Fifo_testbench.vhd
--- Project Name:  Fifo
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: Fifo
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
+----------------------------------------------------------------------------------
+-- Author: Ariel Podlubne
+-- Task: Design and verification of a FIFO.
+-- Reason: Excersie pre-interview (Universita di Bologna - Prof. Benini, Prof. Rossi)
+
+-- Component: Fifo's testbench
+-- Description: It mimics the input signals from the example.
+----------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
  
 ENTITY Fifo_testbench IS
 END Fifo_testbench;
@@ -99,24 +76,6 @@ BEGIN
       wait for 11 ns;	
 		rst_n <= '1';
 		wait for clk_period;
-
---		push_valid_i <= '1';
---		push_data_i <= "1001";
---		wait for clk_period;
---		push_data_i <= "1010";
---		wait for clk_period;
---		push_data_i <= "1011";
---		wait for clk_period;
---		push_data_i <= "1100";
---		pop_grant_i <= '1';
---		wait for clk_period;
---		-- FIFO is full
---		push_data_i <= "1101";
---		wait for clk_period;
---		push_valid_i <= '0';
---		-- ** FIFO is FULL at this point. Now it is emptied ** --
---		--pop_grant_i <= '1';
---		wait for 4*clk_period;
 
 		-- ** Mimics example from exercise ** --
 		pop_grant_i <= '0';
